62c2b8d08e421b51c18c04abfe9f8356db20e2a6
Askar Karasaev <askar.karasaev@gmail.com>
2021-08-15 00:58:15 +0900

    Adding strapi

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,20 +6,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -30,6 +26,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/strapi',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -39,6 +36,10 @@ export default {
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
   ],
+  strapi: {
+    entities: ['restaurants', 'categories'],
+    url: process.env.API_URL,
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
@@ -46,11 +47,10 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <ul v-else>
+      <li v-for="category in categories" :key="category.id">
+        {{ category.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'App',
+  data() {
+    return {
+      categories: [],
+      error: null,
+    }
+  },
+  async mounted() {
+    try {
+      this.categories = await this.$strapi.$categories.find()
+    } catch (error) {
+      this.error = error
+    }
+  },
+}
 </script>
